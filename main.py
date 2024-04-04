56942102e38ea254f7fb2a5026d2fdb393a9f480
Totan <totansarker2@gmail.com>
2024-04-05 00:43:15 +0600

    hello

--- main.py
+++ main.py
@@ -1,10 +1,12 @@
 import os
 import subprocess
+import time
 # file1 = subprocess.run(["ls", "-l"])
 
 for i in range(0, 100):
     # script = `{}`
     file1 = subprocess.run(["bash", "script.sh"])
+    time.sleep(5)
 
 
 
