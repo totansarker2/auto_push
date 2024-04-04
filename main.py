8eb956ccb51548f27db81cefa9f76716a236dc9a
Totan <totansarker2@gmail.com>
2024-04-05 00:42:07 +0600

    hello

--- main.py
+++ main.py
@@ -1,7 +1,13 @@
 import os
 import subprocess
 # file1 = subprocess.run(["ls", "-l"])
-file1 = subprocess.run(["bash", "script.sh"])
+
+for i in range(0, 100):
+    # script = `{}`
+    file1 = subprocess.run(["bash", "script.sh"])
+
+
+
 
 # print("The exit code was: %d" % file1.returncode)
 print("Current Working Directory",os.getcwd())
