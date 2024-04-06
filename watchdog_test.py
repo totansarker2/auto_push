12b24cc1fae820937b7cb2c4f117b8233db8bafa
Totan <totansarker2@gmail.com>
2024-04-06 15:54:48 +0600

    a20Tk0

--- watchdog_test.py
+++ watchdog_test.py
@@ -1,22 +1,39 @@
-import sys
-import time
-import logging
+######Import for Watchdog
 from watchdog.observers import Observer
-from watchdog.events import LoggingEventHandler
+from watchdog.events import FileSystemEventHandler
 
-if __name__ == "__main__":
-    logging.basicConfig(level=logging.INFO,
-                        format='%(asctime)s - %(message)s',
-                        datefmt='%Y-%m-%d %H:%M:%S')
-    path = sys.argv[1] if len(sys.argv) > 1 else '.'
-    logging.info(f'start watching directory {path!r}')
-    event_handler = LoggingEventHandler()
-    observer = Observer()
-    observer.schedule(event_handler, path, recursive=True)
-    observer.start()
-    try:
-        while True:
-            time.sleep(1)
-    finally:
-        observer.stop()
-        observer.join()
+######Import for Scpriting#####
+import subprocess
+import time
+from random import randint
+
+def git_Script():
+    commit_text = f"a20Tk{0}"
+    value = randint(5, 7)
+    file1 = subprocess.run(["bash", "script.sh", commit_text])
+    time.sleep(value)
+
+class MyEventHandler(FileSystemEventHandler):
+    def on_modified(self, event):
+        # print(event.src_path, "modified.")
+        git_Script()
+
+    def on_created(self, event):
+        print(event.src_path, "created.")
+
+    def on_moved(self, event):
+        print(event.src_path, "moved to", event.dest_path)
+
+    def on_deleted(self, event):
+        print(event.src_path, "deleted.")
+
+
+observer = Observer()
+observer.schedule(MyEventHandler(), ".", recursive=False)
+observer.start()
+try:
+    while observer.is_alive():
+        observer.join(1)
+except KeyboardInterrupt:
+    observer.stop()
+observer.join()
