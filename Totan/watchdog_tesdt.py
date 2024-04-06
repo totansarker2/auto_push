ce21cc9a2ed0006e03fbda9e61846194df3524ad
Totan <totansarker2@gmail.com>
2024-04-06 21:37:40 +0600

    a20Tk0

--- Totan/watchdog_tesdt.py
+++ Totan/watchdog_tesdt.py
@@ -40,6 +40,7 @@ class MyEventHandler(FileSystemEventHandler):
             self.counter+=1
         self.file_cache[key] = True
         
+        ###
         if self.counter >= 1:
             # os.system('\x03')
             os.kill(os.getpid(), signal.SIGINT)
